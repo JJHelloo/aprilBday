60cde365f4cd064482680f95ff016860a479785e
Justin Johnson <94727825+JJHelloo@users.noreply.github.com>
2024-04-03 15:20:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('openBtn').addEventListener('click', function() {
+    var card = document.getElementById('card');
+    card.style.transform = "rotateY(180deg)"; // Simulates the card opening
+});
